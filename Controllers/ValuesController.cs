17da5da37d16b71fc1d09cdbd9b97bca4a90329e
Muhammad Hamza Mushtaq <70532236+hamzatoor251@users.noreply.github.com>
2022-08-04 16:00:27 +0500

    Add files via upload

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication3.DataLayer;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public List<Student> Get()
+        {
+            SQLDataHelper sqlData = new SQLDataHelper();
+            
+            return sqlData.GetStudentData();
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DataLayer/SQLDataHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using WebApplication3.Models;
+using System.Runtime.Caching;
+
+namespace WebApplication3.DataLayer
+{
+    public class SQLDataHelper
+    {
+        string cs = "";
+        public SQLDataHelper()
+        {
+            cs = WebConfigurationManager.ConnectionStrings["DBSC"].ConnectionString;
+        }
+        public List<Student> GetStudentData()
+        {
+            ObjectCache cache = MemoryCache.Default;
+            if (cache["students"]==null)
+            {
+                var cacheItemPolicy = new CacheItemPolicy
+                {
+                    AbsoluteExpiration = DateTimeOffset.Now.AddDays(1)
+                };
+                SQLDataHelper sqlData = new SQLDataHelper();
+                var cacheItem = new CacheItem("students", sqlData.GetStudentData());
+                cache.Add(cacheItem, cacheItemPolicy);
+            }
+            return (List<Student>)cache.Get("students");
+            //List<Student> lstStudent = new List<Student>();
+
+            //using (SqlConnection con = new SqlConnection(cs))
+            //{
+            //    string qeury = "select * From Student";
+
+            //    SqlCommand cmd = new SqlCommand(qeury, con);
+
+            //    con.Open();
+            //    SqlDataReader rdr = cmd.ExecuteReader();
+
+            //    while (rdr.Read())
+            //    {
+            //        Student std = new Student();
+
+            //        std.ID = Convert.ToString(rdr["ID"]);
+            //        std.Name = (rdr["Name"].ToString());
+            //        //std.Email = rdr["Email"].ToString();
+            //        std.Class = rdr["Class"].ToString();
+            //        lstStudent.Add(std);
+            //    }
+            //    con.Close();
+            //}
+            //return lstStudent;
+        }
+    }
+}
